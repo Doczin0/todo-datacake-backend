a8d16851a03cf4a030589d369983efa72aa97fcb
Davi Henri Grecco <davi.grecco2015@gmail.com>
2025-10-26 14:35:19 -0300

    chore: logging, e-mail e melhorias de configuração

--- server/settings.py
+++ server/settings.py
@@ -161,7 +161,7 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "no-reply@datacake.local")
 
-# --- Logging estruturado ---
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
